ef0f557a9ba84d77defb35ac8fb25249afb7aa3b
Kylian <hsjgc1234@gmail.com>
2024-10-13 16:42:45 +0900

    qr scan

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
 
-// class LoadingScreen extends StatelessWidget {
-//   const LoadingScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: const Color.fromRGBO(0xDB, 0x1E, 0x17, 1),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Image.asset('assets/logo.png', width: 250),
-//             ElevatedButton(
-//               child: const Text('시작하기'),
-//               onPressed: () {
-//                 Navigator.pushNamed(context, '/qr_scan');
-//                 // Navigator.pushNamed(context, '/scanner');
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+// qr code 받기 위해 StatefulWidget 으로 수정
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
@@ -58,6 +33,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 }
 
+// pop 된 qr code SnackBar 로 띄워주기
 Future<void> _navigateAndDisplaySelection(BuildContext context) async {
   // Navigator.push returns a Future that completes after calling
   // Navigator.pop on the Selection Screen.
